1997a7582a4cd157de6353819ed12814187a0cb5
Qobil Urinboev <qobilurinboev98@gmail.com>
2022-04-04 00:13:54 +0500

    tabs

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -38,10 +38,10 @@ const modals = () => {
 
 
 
-    
+
     bindModal('.popup_engineer_btn', ".popup_engineer", ".popup_engineer .popup_close");
     bindModal('.phone_link', '.popup', '.popup');
-    showModalByTime('.popup', 3000)
+    // showModalByTime('.popup', 60000)
 };
 
 export default modals
